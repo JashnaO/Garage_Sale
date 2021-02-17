4ac1c41b28e0a013eb81db54f1f00179b36b813d
JashnaO <77934083+JashnaO@users.noreply.github.com>
2021-02-17 09:46:18 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function go_to_furniture() {
+    window.location="Macintosh HD. Users.taraloza.Downloads.Garage Sale.furniture.furniture.html";
+    
+}
+
+function go_to_books() {
+    window.location="books/books.html";
+}
+
+function go_to_boardGames() {
+    window.location="boardGames/boardGames.html";
+}
+
+function go_to_toys() {
+    window.location="toys/toys.html";
+}
